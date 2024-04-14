f08fa06a03f7178b0f5f5bc99f07b632ba945f4e
fennore <29664945+fennore@users.noreply.github.com>
2024-04-14 16:16:10 +0000

    Prep build

---
+++ js/app.d.ts
@@ -0,0 +1,7 @@
+import { LitElement } from 'lit';
+export declare class DayProductionChart extends LitElement {
+    static styles: import("lit").CSSResult;
+    title: string;
+    render(): import("lit-html").TemplateResult<1>;
+}
+//# sourceMappingURL=app.d.ts.map
