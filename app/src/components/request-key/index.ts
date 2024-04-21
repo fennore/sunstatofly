0c4b7da069cb54dd542f399ef7718c8576b514ed
fennore <29664945+fennore@users.noreply.github.com>
2024-04-21 17:59:32 +0000

    Fix input

--- app/src/components/request-key/index.ts
+++ app/src/components/request-key/index.ts
@@ -45,8 +45,10 @@ export class RequestKey extends LitElement {
     override render() {
         return html`<form>
             <key-wrapper>
-                <key-icon-button slot="trailing-icon" />
-                <md-filled-button type="submit">Naar dashboard</md-filled-button>
+                <md-outlined-text-field name="key" type="password" label="Toegangssleutel" placeholder="Geef de toegangssleutel in">
+                    <key-icon-button slot="trailing-icon" />
+                    <md-filled-button type="submit">Naar dashboard</md-filled-button>
+                </md-outlined-text-field>
             </key-wrapper>
         </form>`
     }
