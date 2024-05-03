1aeadb5e238e18c736d7888dc69fc86651e50f9d
fennore <29664945+fennore@users.noreply.github.com>
2024-05-03 16:42:18 +0200

    zz

--- app/src/components/charts/day-production-chart.ts
+++ app/src/components/charts/day-production-chart.ts
@@ -1,14 +1,14 @@
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators';
-import {RefOrCallback, ref} from 'lit/directives/ref';
-import { EChartsType, init } from 'echarts';
+import { LitElement, html, css } from "lit";
+import { customElement, property } from "lit/decorators";
+import { RefOrCallback, ref } from "lit/directives/ref";
+import { EChartsType, init } from "echarts";
 
-@customElement('day-production-chart')
+@customElement("day-production-chart")
 export class DayProductionChart extends LitElement {
   #wrapper?: HTMLElement;
   #chart?: EChartsType;
 
-  static override styles=css`
+  static override styles = css`
     div.chart-wrapper {
       width: 100%;
       height: 100%;
@@ -20,9 +20,9 @@ export class DayProductionChart extends LitElement {
   constructor() {
     super();
 
-    this.addEventListener('resize', () => {
+    this.addEventListener("resize", () => {
       this.#chart?.resize();
-    })
+    });
   }
 
   // Declare reactive properties
@@ -31,44 +31,51 @@ export class DayProductionChart extends LitElement {
 
   assignChart = (element: HTMLElement) => {
     this.#wrapper = element;
-    console.log('assigning', element);
-    if(element && false) {
+    console.log("assigning", element);
+
+    if (element && false) {
       this.#chart = init(element);
       this.#chart.setOption({
         legend: {},
         tooltip: {},
         dataset: {
           source: [
-            ['time', 'today', 'yesterday'],
-            ['test', 10, 20],
-            ['test2', 15, 18],
-            ['test3', 22, 8],
-            ['test4', 18, 12]
+            ["time", "today", "yesterday"],
+            ["test", 10, 20],
+            ["test2", 15, 18],
+            ["test3", 22, 8],
+            ["test4", 18, 12]
           ]
         },
         // Declare an x-axis (category axis).
         // The category map the first column in the dataset by default.
-        xAxis: { type: 'category' },
+        xAxis: { type: "category" },
         // Declare a y-axis (value axis).
         yAxis: {},
         // Declare several 'bar' series,
         // every series will auto-map to each column by default.
-        series: [{ type: 'line', areaStyle: {} }, { type: 'line', areaStyle: {} }]
+        series: [
+          { type: "line", areaStyle: {} },
+          { type: "line", areaStyle: {} }
+        ]
       });
     }
-  }
+  };
 
   // Render the UI as a function of component state
   override render() {
-    console.log('rendering', this.stats);
+    console.log("rendering", this.stats);
     return html`
-        <div class="chart-wrapper" ${ref(this.assignChart as RefOrCallback<Element>)}></div>
+      <div
+        class="chart-wrapper"
+        ${ref(this.assignChart as RefOrCallback<Element>)}
+      ></div>
     `;
   }
 
   override connectedCallback(): void {
-    console.log('connect', this.#wrapper);
-    if(this.#wrapper) {
+    console.log("connect", this.#wrapper);
+    if (this.#wrapper) {
       this.#chart = init(this.#wrapper);
       this.#chart?.showLoading();
       this.#chart.setOption({
@@ -76,21 +83,24 @@ export class DayProductionChart extends LitElement {
         tooltip: {},
         dataset: {
           source: [
-            ['time', 'today', 'yesterday'],
-            ['test', 10, 20],
-            ['test2', 15, 18],
-            ['test3', 22, 8],
-            ['test4', 18, 12]
+            ["time", "today", "yesterday"],
+            ["test", 10, 20],
+            ["test2", 15, 18],
+            ["test3", 22, 8],
+            ["test4", 18, 12]
           ]
         },
         // Declare an x-axis (category axis).
         // The category map the first column in the dataset by default.
-        xAxis: { type: 'category' },
+        xAxis: { type: "category" },
         // Declare a y-axis (value axis).
         yAxis: {},
         // Declare several 'bar' series,
         // every series will auto-map to each column by default.
-        series: [{ type: 'line', areaStyle: {} }, { type: 'line', areaStyle: {} }]
+        series: [
+          { type: "line", areaStyle: {} },
+          { type: "line", areaStyle: {} }
+        ]
       });
     }
   }
@@ -101,19 +111,19 @@ export class DayProductionChart extends LitElement {
   }
 
   override updated(changedProperties: Map<string, any>) {
-    if (changedProperties.has('stats')) {
-      console.log('stats updated');
-      if(!this.stats) {
+    if (changedProperties.has("stats")) {
+      console.log("stats updated");
+      if (!this.stats) {
         // Set loading
         this.#chart?.showLoading();
       } else {
         // Set data
-        console.log('day prod stats', this.stats);
+        console.log("day prod stats", this.stats);
         this.#chart?.setOption({
           dataset: {
             source: this.stats
           }
-        })
+        });
         this.#chart?.hideLoading();
       }
     }
