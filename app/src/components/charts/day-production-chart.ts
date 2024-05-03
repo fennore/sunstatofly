a7b899d2bb2c80b1ac04f3563b29d095775dd672
fennore <29664945+fennore@users.noreply.github.com>
2024-05-03 22:49:58 +0200

    zz

--- app/src/components/charts/day-production-chart.ts
+++ app/src/components/charts/day-production-chart.ts
@@ -1,7 +1,9 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators";
 import { RefOrCallback, ref } from "lit/directives/ref";
-import { EChartsType, init } from "echarts";
+import * as echarts from "echarts";
+
+type EChartsOption = echarts.EChartsOption;
 
 @customElement("day-production-chart")
 export class DayProductionChart extends LitElement {
@@ -10,6 +12,7 @@ export class DayProductionChart extends LitElement {
 
   static override styles = css`
     div.chart-wrapper {
+      display: block;
       width: 100%;
       height: 100%;
       min-height: 250px;
@@ -33,8 +36,8 @@ export class DayProductionChart extends LitElement {
     this.#wrapper = element;
     console.log("assigning", element);
 
-    if (element && false) {
-      this.#chart = init(element);
+    if (element) {
+      this.#chart = echarts.init(element);
       this.#chart?.setOption({
         legend: {},
         tooltip: {},
@@ -51,7 +54,7 @@ export class DayProductionChart extends LitElement {
         // The category map the first column in the dataset by default.
         xAxis: { type: "category" },
         // Declare a y-axis (value axis).
-        yAxis: {},
+        yAxis: { type: "value" },
         // Declare several 'bar' series,
         // every series will auto-map to each column by default.
         series: [
@@ -61,49 +64,6 @@ export class DayProductionChart extends LitElement {
       });
     }
   };
-
-  // Render the UI as a function of component state
-  override render() {
-    console.log("rendering", this.stats);
-    
-    return html`
-      <div class="chart-wrapper" ${ref(this.assignChart as RefOrCallback<Element>)}></div>
-    `;
-  }
-
-  override connectedCallback(): void {
-    super.connectedCallback();
-
-    console.log("connect", this.#wrapper);
-    if (this.#wrapper) {
-      this.#chart = init(this.#wrapper);
-      this.#chart?.showLoading();
-      this.#chart?.setOption({
-        legend: {},
-        tooltip: {},
-        dataset: {
-          source: [
-            ["time", "today", "yesterday"],
-            ["test", 10, 20],
-            ["test2", 15, 18],
-            ["test3", 22, 8],
-            ["test4", 18, 12]
-          ]
-        },
-        // Declare an x-axis (category axis).
-        // The category map the first column in the dataset by default.
-        xAxis: { type: "category" },
-        // Declare a y-axis (value axis).
-        yAxis: {},
-        // Declare several 'bar' series,
-        // every series will auto-map to each column by default.
-        series: [
-          { type: "line", areaStyle: {} },
-          { type: "line", areaStyle: {} }
-        ]
-      });
-    }
-  }
 
   override disconnectedCallback(): void {
     super.disconnectedCallback();
@@ -130,5 +90,14 @@ export class DayProductionChart extends LitElement {
         this.#chart?.hideLoading();
       }
     }
+  }
+
+  // Render the UI as a function of component state
+  override render() {
+    console.log("rendering", this.stats);
+    
+    return html`
+      <div class="chart-wrapper" ${ref(this.assignChart as RefOrCallback<Element>)}></div>
+    `;
   }
 }
