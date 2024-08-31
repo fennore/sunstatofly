8d5426e1007bd1d3450cc187a813f461854fb233
fennore <29664945+fennore@users.noreply.github.com>
2024-08-31 16:59:20 +0000

    Rename notification to notice

--- app/src/components/request-key/notice.ts
+++ app/src/components/request-key/notice.ts
@@ -1,8 +1,8 @@
 import { LitElement, css, html } from "lit";
 import { customElement } from "lit/decorators";
 
-@customElement('notification')
-export class Notification extends LitElement {
+@customElement('notice')
+export class Notice extends LitElement {
     static override styles = css`
         :host {
             border: 1px solid #88b;
