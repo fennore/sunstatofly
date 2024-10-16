7e4593852bade0b8932dae6fe534d3b001046a0f
fennore <29664945+fennore@users.noreply.github.com>
2024-10-16 08:34:37 +0200

    Fix for typescript validation

--- app/src/utils/getData.ts
+++ app/src/utils/getData.ts
@@ -25,6 +25,61 @@ const requestMap: Map<string, string> = new Map([
     ['weather', `[domain]/plant/getWeatherNew`],
 ]);
 
+
+/**
+ * Get plant local date.
+ */
+const getPlantDate: (tz: number, date: Date) => Date = (plantTZ = 2, date) => {
+    const plantDate = new Date(date);
+    plantDate.setHours(date.getHours() + Math.round(date.getTimezoneOffset()/60) + plantTZ);
+
+    return plantDate;
+}
+
+/**
+ * Format Date to yyyy-m-d string.
+ */
+const getYmd: (date: Date) => string = date => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+/**
+ * Get today date in string format.
+ */
+const getToday: (tz?: number) => string = (plantTZ = 2) => {
+    const plantToday = getPlantDate(plantTZ, new Date());
+    // yyyy-m-d
+    return getYmd(plantToday);
+}
+
+/**
+ * Get yesterday date in string format.
+ */
+const getYesterday: (tz?: number) => string = (plantTZ = 2) => {
+    const plantYesterday = getPlantDate(plantTZ, new Date());
+    plantYesterday.setDate(plantYesterday.getDate() - 1);
+    // yyyy-m-d
+    return getYmd(plantYesterday);
+}
+
+/**
+ * Get last month date in string format.
+ */
+const getLastMonth: (tz?: number) => string = (plantTZ = 2) => {
+    const plantLastMonth = getPlantDate(plantTZ, new Date());
+    plantLastMonth.setMonth(plantLastMonth.getMonth() - 1, 1);
+    // yyyy-m-d
+    return getYmd(plantLastMonth);
+}
+
+/**
+ * Get last year date in string format.
+ */
+const getLastYear: (tz?: number) => string = (plantTZ = 2) => {
+    const plantLastYear = getPlantDate(plantTZ, new Date());
+    plantLastYear.setFullYear(plantLastYear.getFullYear() - 1, 0, 1);
+    // yyyy-m-d
+    return getYmd(plantLastYear);
+}
+
 export default (uid: string) => {
 
     const getUrl: (url?:string) => URL = (url) => {
